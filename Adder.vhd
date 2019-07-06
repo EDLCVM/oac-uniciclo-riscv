f0818a2eec1d668ed322e4bd3689cc17a9d5cc05
Éden Medeiros <edenlcvm@gmail.com>
2019-07-05 22:03:15 -0300

    Criação do comportamento do Adder

--- Adder.vhd
+++ Adder.vhd
@@ -1,6 +1,21 @@
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+
 entity Adder is
+	generic (WSIZE : natural := 32);
+	port (
+		A, B : in std_logic_vector(WSIZE - 1 downto 0);
+		saida : out std_logic_vector(WSIZE - 1 downto 0)
+	);
 end Adder;
 
 architecture comportamento of Adder is
-begin
+
+	begin
+	process(A, B)
+		begin
+		saida <= std_logic_vector(signed(A) + signed(B));
+	end process;
+
 end comportamento;
--- riscv_pkg.vhd
+++ riscv_pkg.vhd
@@ -1,3 +1,6 @@
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
 
 package riscv_pkg is
 type ULA_OP is (ADD_OP, SUB_OP, AND_OP,   OR_OP,  XOR_OP, SLL_OP, SRL_OP,
@@ -5,5 +8,12 @@ type ULA_OP is (ADD_OP, SUB_OP, AND_OP,   OR_OP,  XOR_OP, SLL_OP, SRL_OP,
 					 
 -- inserir component aqui.
 
+component Adder
+	generic (WSIZE : natural := 32);
+	port (
+		A, B : in std_logic_vector(WSIZE - 1 downto 0);
+		saida : out std_logic_vector(WSIZE - 1 downto 0)
+	);
+end component;
 
 end riscv_pkg;
